ca82c289ad2a79fdc494268c1786035712ca9631
Klevens <kelvinreyescnh@gmail.com>
2023-02-03 14:16:11 -0400

    adding reverse writing

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
-const text = document.getElementById("dynamicH1")
+//H1 que tiene el texto que buscamos que cambie
+const text = document.getElementById("dynamicH1");
 
-let str = ["Fiera", "Mastodonte", "Eminencia"]
+// H1 que conitiene el pipe que parpadea
+const rayita = document.getElementById("rayita");
+
+let str = ["Fiera", "Mastodonte", "Eminencia"];
 
 const speed = 120;
 const waitTime = 2000;
 let i = 0;
 let x = 0;
 
-const typeWriter = () => {
-    console.log(str[x])
-    if ( i < str[x]?.length) {
-        text.innerHTML += str[x].charAt(i);
-        i++;
-        setTimeout(typeWriter, speed);
-      } else if ((text.innerHTML.length === str[x]?.length) && (x <= str.length)) {
-        x++;
-        i=0;
-        setTimeout(()=>{
-          text.innerHTML = ""  
-          typeWriter()
-        }, waitTime);
-      } else if (str[x] === undefined) {
-        i = 0;
-        x = 0;
-        typeWriter()
-      }
-    console.log(text.innerHTML)
+const removeTypeWriter = (str) => {
+  let nstr = str;
+  let result = "";
+  if (str?.length) {
+    result = nstr.slice(0, -1);
+    text.innerHTML = result;
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(removeTypeWriter(result));
+      }, speed);
+    });
+  }
+  return "Esta es una bombaa";
 };
 
+const typeWriter = () => {
+  if (i < str[x]?.length) {
+    text.innerHTML += str[x].charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else if (text.innerHTML.length === str[x]?.length && x <= str.length) {
+    x++;
+    i = 0;
+    rayita.classList.add("parpadea");
+    setTimeout(async () => {
+      rayita.classList.remove("parpadea");
+      let kelvin = await removeTypeWriter(text.innerHTML);
+      typeWriter();
+    }, waitTime);
+  } else if (str[x] === undefined) {
+    i = 0;
+    x = 0;
+    typeWriter();
+  }
+  console.log(text.innerHTML);
+};
 
 setTimeout(typeWriter, speed);
-
-
-
