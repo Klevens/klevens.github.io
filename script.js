c0882686fb6d805326ecabb7403aa1e87397e3ec
klevens <kelvinreyescnh@gmail.com>
2023-02-09 16:50:27 -0400

    adding contact form

--- script.js
+++ script.js
@@ -46,7 +46,46 @@ const typeWriter = () => {
     x = 0;
     typeWriter();
   }
- 
 };
 
 setTimeout(typeWriter, speed);
+
+// Formspreee controlador
+
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        status.className = "success";
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, "errors")) {
+            status.innerHTML = data["errors"]
+              .map((error) => error["message"])
+              .join(", ");
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+          }
+          status.className = "error";
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+      status.className = "error";
+    });
+}
+form.addEventListener("submit", handleSubmit);
